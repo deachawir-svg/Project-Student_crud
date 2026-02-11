d0f312d7fba0f081a28b81920168c90bbd894ff1
deachawir-svg <deacha.wir@gmail.com>
2026-02-11 21:02:09 +0700

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+
+  {
+    path: '/student',
+    name: 'student',
+    component: () => import('../views/Student_crud.vue')
   }
+
 ]
 
 const router = createRouter({
